bc873a38fd0c29f1a866bcf42e738ad5321a4047
sally <sal_a90@yahoo.com>
2022-09-01 09:52:12 -0300

    1.2.1

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ client.on("ready", async () => {
   console.log(`${client.user.tag} loaded`)
   const presences = [`APS@${client.user.id}`, `APS`, `.help`]
   client.user.setStatus('idle')
-  if (process.env.PRESENCE !== 3) {
-     client.user.setPresence({activities: [{name: presences[process.env.PRESENCE], type: 3}]})
-  }
+  client.user.setPresence({activities: [{name: presences[process.env.PRESENCE], type: 3}]})
   console.log("Updated rich presence")
 })
 
